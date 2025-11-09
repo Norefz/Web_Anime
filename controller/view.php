18d1e6f3d66ae1305560589b3941f53ede51285f
erlan <noref.kyoref@gmail.com>
2025-11-09 10:52:12 +0700

    add display error

---
+++ controller/view.php
@@ -0,0 +1,24 @@
+<?php   
+//to display_error
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+include 'core/connect.php';
+$sql = "SELECT * FROM anime";
+$result = mysqli_query($connect, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  while ($row = mysqli_fetch_assoc($result)){
+    echo $row["id_anime"] . "<br>";
+    echo $row["title"] . "<br>";
+    echo $row["synopsis"] . "<br>";
+    echo $row["genre"] . "<br>";
+    echo $row["date"] . "<br>";
+    echo $row["rating"] . "<br>";
+  }
+}
+
+
+?>
+
+
