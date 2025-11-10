23ed37c3384dffec1559e0a0cc1b324942489a74
erlan <noref.kyoref@gmail.com>
2025-11-10 11:21:47 +0700

    add variable and sql

--- controller/CRUD/create.php
+++ controller/CRUD/create.php
@@ -1,6 +1,16 @@
 <?php  
 include'../../core/connect.php';
 include'../../core/error.php';
+if (isset($_POST['submit'])) {
+  $image = $_POST['image'];
+  $title= $_POST['title'];
+  $synopsis = $_POST['synopsis'];
+  $genre = $_POST['genre'];
+  $date = $_POST['date'];
+  $rating = $_POST['rating'];
+  $result = mysql_query($conn,"INSERT INTO anime(image,title,synopsis,genre,date,rating)");
+  # code...
+}
 
 
 ?>
