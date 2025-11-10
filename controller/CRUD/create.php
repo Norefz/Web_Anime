61258674f3958db6c67c1e9f2959496a0820679c
erlan <noref.kyoref@gmail.com>
2025-11-10 13:03:59 +0700

    creating new file (edit_anim)

--- controller/CRUD/create.php
+++ controller/CRUD/create.php
@@ -1,6 +1,5 @@
 <?php  
 include'../../core/connect.php';
-include'../../core/error.php';
 if (isset($_POST['submit'])) {
   $title= $_POST['title'];
   $image = $_POST['image'];
@@ -8,11 +7,12 @@ if (isset($_POST['submit'])) {
   $genre = $_POST['genre'];
   $date = $_POST['date'];
   $rating = $_POST['rating'];
-  $result = mysql_query($conn,"INSERT INTO anime(image,title,synopsis,genre,date,rating)
-    VALUES('$title','$synopsis','$genre','$date','$rating')";
-  mysql_query($conn,$result);
+    
+  $result ="INSERT INTO anime(title,image,synopsis,genre,date,rating)
+    VALUES('$title','$image','$synopsis','$genre','$date','$rating')";
+  mysqli_query($conn,$result);
   echo"<h1>Anime Added!</h1>";
-}
 
+}
 
 ?>
