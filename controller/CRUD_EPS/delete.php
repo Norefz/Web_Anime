09ec3d03702bd81ef44b04857dec2429884a3190
erlan <noref.kyoref@gmail.com>
2025-11-14 12:50:37 +0700

    add delete option for episode

---
+++ controller/CRUD_EPS/delete.php
@@ -0,0 +1,15 @@
+<?php  
+include'../../core/error.php';
+include'../../core/connect.php';
+if (isset($_GET['id_anime'])) {
+  $id = $_GET['id'];
+  $sql = "DELETE FROM episode WHERE id_anime=$id";
+  if (mysqli_query($conn,$sql)) {
+    echo"data $id has  been succesfully deleted";
+    //header('location:../../view/index.php');
+  }
+  else {
+    echo"data failed to delete!";
+
+  }
+}
